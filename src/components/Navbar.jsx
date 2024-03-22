5d945ae288750fdbf19a622de56a253c856d2686
Luis Gomez <luisgomez@MacBook-Pro.local>
2024-03-21 19:45:51 -0700

    finished moving navbar to top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,19 @@ import group from '../assets/groups.svg'
 import notes from '../assets/notesIcon.svg'
 import settingsIcon from '../assets/settingsIcon.svg'
 import messageIcon from '../assets/messageIcon.svg'
+import pen from '../assets/pen.png'
 import SettingsModal from './SettingsModal'
 import { signOut } from 'firebase/auth'
 import { auth, db } from '../firebase'
 import { AuthContext } from '../context/AuthContext'
+import { TabContext } from '../context/TabContext'
+
 
 
 function Navbar() {
   const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
   const { currentUser } = useContext(AuthContext);
-  const [activeTab, setActiveTab] = useState('settings');
+  const { setActiveTab } = useContext(TabContext);
 
   const openSettingsModal = () => setIsSettingsModalOpen(true);
   const closeSettingsModal = () => setIsSettingsModalOpen(false);
@@ -25,6 +28,7 @@ function Navbar() {
 
   return (
     <div className="navbar">
+      <img src={pen}></img>
       <span className="nav-logo">Pen Pals</span>
       <div className='navbar-Icons'>
         <button className='navIcon' onClick={() => setActiveTab('personal')}>
@@ -45,246 +49,9 @@ function Navbar() {
         <span> {currentUser.displayName} </span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
-      {/* <div>
-        {activeTab === 'personal' && <Messages />}
-        {activeTab === 'group' && <Groups />}
-        {activeTab === 'notes' && <Notes />}
-      </div> */}
       {isSettingsModalOpen && <SettingsModal onClose={closeSettingsModal} />}
     </div>
   )
-}
-
-
-function Messages() {
-  const [chats, setChats] = useState([])
-  const {currentUser} = useContext(AuthContext);
-  const { dispatch } =  useContext(ChatContext);
-
-
-  useEffect(() => {
-    const getChats = () => {
-    const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
-      setChats(doc.data());
-    });
-
-    return () => {
-      unsub()
-    };
-  };
-    currentUser.uid && getChats();
-  },[currentUser.uid])
-
-  const handleSelect = (u) => {
-    dispatch({type:"CHANGE_USER", payload: u})
-  }
-
-  console.log(chats)
-  
-  if(chats === undefined) {
-    return ( 
-    <>
-      <div>
-          You currently have no messages. Create a new message by searching for a user above.  
-      </div>   
-    </> 
-    )
-  } else if (Object.entries(chats).length === 0) {
-    return ( 
-    <>
-      <div>
-        You currently have no messages. Create a new message by searching for a user above.  
-      </div>   
-    </>
-    )
-  } else {
-    console.log(chats)
-    return (
-      <div className="chats">
-      {Object.entries(chats)?.sort((a, b) => b[1].date - a[1].date).map(chat => (
-      <div className="userChat" key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)}>
-        {/* <img src={chat[1].userInfo.photoURL} alt="" className="userChat" /> */}
-        <div className="userChatInfo">
-          <span>{chat[1].userInfo.displayName}</span>
-          <p>{chat[1].lastMessage?.text.slice(0, 20)}{chat[1].lastMessage?.text.length > 20 ? '...' : ''}</p>
-        </div>
-      </div>
-    ))}
-  </div>
-    )
-  }
-}
-
-function Groups() {
-  const [chats, setChats] = useState([])
-  const {currentUser} = useContext(AuthContext);
-  const { dispatch } =  useContext(ChatContext)
-
-
-  useEffect(() => {
-    const getChats = () => {
-    const unsub = onSnapshot(doc(db, "userGroups", currentUser.uid), (doc) => {
-      setChats(doc.data());
-      console.log(doc.data())
-    });
-
-    return () => {
-      unsub()
-    };
-  };
-    currentUser.uid && getChats();
-  },[currentUser.uid])
-
-  const handleSelect = (u) => {
-    dispatch({type:"CHANGE_GROUP", payload: u})
-  }
-
-  console.log(chats);
-  const test3 = Object.entries(chats)
-  console.log(test3)
-
-  if(chats != undefined) {
-    console.log(Object.entries(chats))
-    const test = Object.entries(chats)
-    const test2 = test[1]
-    console.log(test2)
-  // Object.entries(chats)?.sort((a, b) => b[1].date - a[1].date).map(chat => (
-  // console.log(Object.entries(chats)[1])))
-  console.log(test.length)
-  }
-
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-  
-
-  if(chats === undefined) {
-    return ( 
-    <>
-        <button onClick={openModal}>New Group</button>
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-          <GroupChatForm onClose={closeModal} />
-        </Modal> 
-      <div>
-      You are currently in no group chats. Create a new group above.  
-      </div>   
-    </> 
-    )
-  } else if (Object.entries(chats).length === 0) {
-    return(
-      <>
-        <button onClick={openModal}>New Group</button>
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-          <GroupChatForm onClose={closeModal} />
-        </Modal> 
-        <div>
-          You are currently in no group chats. Create a new group above.  
-        </div>   
-      </>
-
-    )
-  } else {
-    return (
-      <>
-        <button onClick={openModal}>New Group</button>
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-          <GroupChatForm onClose={closeModal} />
-        </Modal>    
-        <div className="chats">
-      {Object.entries(chats)?.sort((a, b) => b[1].date - a[1].date).map(chat => (
-      <div className="userChat" key={chat[0]} onClick={() => handleSelect(chat[1].groupInfo)}>
-        {/* <img src={chat[1].groupInfo.photoURL} alt="" className="userChat" /> */}
-        <div className="userChatInfo">
-          {/* <span>{chat[1].groupInfo.groupName}</span> */}
-          <p>{chat[1].lastMessage?.text.slice(0, 20)}{chat[1].lastMessage?.text.length > 20 ? '...' : ''}</p>
-        </div>
-      </div>
-      ))}
-
-    </div>
-    </>
-  )
-
-  }
-}
-
-function Notes() {
-  const [chats, setChats] = useState([])
-  const {currentUser} = useContext(AuthContext);
-  const { dispatch } =  useContext(ChatContext)
-
-
-  useEffect(() => {
-    const getChats = () => {
-    const unsub = onSnapshot(doc(db, "userNotes", currentUser.uid), (doc) => {
-      setChats(doc.data());
-      console.log(doc.data())
-    });
-
-    return () => {
-      unsub()
-    };
-  };
-    currentUser.uid && getChats();
-  },[currentUser.uid])
-
-  const handleSelect = (u) => {
-    dispatch({type:"CHANGE_NOTE", payload: u})
-  }
-
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
-  
-  if(chats === undefined) {
-    return ( 
-    <>
-        <button onClick={openModal}>New Note</button>
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-          <NewNotesForm onClose={closeModal} />
-        </Modal>    
-      <div>
-          You currently have no notes.  
-      </div>   
-    </> 
-    )
-  } else if (Object.entries(chats).length === 0) {
-    return(
-      <>
-        <button onClick={openModal}>New Note</button>
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-          <NewNotesForm onClose={closeModal} />
-        </Modal> 
-        <div>
-          You currently have no notes.  
-        </div>   
-      </>
-
-    )
-  } else {
-  return (
-    <>
-      <button onClick={openModal}>New Note</button>
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <NewNotesForm onClose={closeModal} />
-      </Modal> 
-      <div className="chats">
-        {Object.entries(chats)?.sort((a, b) => b[1].date - a[1].date).map(chat => (
-        <div className="userChat" key={chat[0]} onClick={() => handleSelect(chat[1].noteInfo)}>
-          {/* <img src={chat[1].noteInfo.photoURL} alt="" className="userChat" /> */}
-          <div className="userChatInfo">
-            <span>{chat[1].noteInfo.noteName}</span>
-            <p>{chat[1].lastMessage?.text.slice(0, 20)}{chat[1].lastMessage?.text.length > 20 ? '...' : ''}</p>
-          </div>
-        </div>
-        ))}
-      </div>
-    </>
-  )
-  }
 }
 
 function UserSettings() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
 import Navbar from '../components/Navbar'
+import { TabProvider } from '../context/TabContext';
 
 function Home() {
   return (
+    <TabProvider>
     <div className="home">
-      {/* <Navbar className="navbar" /> */}
       <div className="home-container">
-        <Navbar className="navbar" />
+        <Navbar className="navbar"/>
         <div className='content-container'>
-        <Sidebar />
+        <Sidebar/>
         <Chat />
         </div>
       </div>
     </div>
+    </TabProvider>
   )
 }
 
